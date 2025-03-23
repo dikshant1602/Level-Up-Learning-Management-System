842c2037cb051464ec76368a5050bd34c893893b
dikshant1602 <dikshantchoudhary12345@gmail.com>
2025-03-23 18:21:17 +0530

    handling auth pages logic continued

--- server/controllers/auth-controller/index.js
+++ server/controllers/auth-controller/index.js
@@ -1,36 +1,73 @@
-const { model } = require('mongoose');
-const User = require('../../models/user-model');
-const bcrypt = require('bcryptjs');
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
-    const {userName, userEmail, password, role } = req.body;
+  const { userName, userEmail, password, role } = req.body;
 
-    const existingUser = await User.findOne({
-        $or : [{userEmail}, {userName}]
+  const existingUser = await User.findOne({
+    $or: [{ userEmail }, { userName }],
+  });
+
+  if (existingUser) {
+    return res.status(400).json({
+      success: false,
+      message: "User name or user email already exists",
     });
+  }
 
-    if (existingUser) {
-        return res.status(400).json({
-            success: false,
-            message: 'User name or user email already exists'
-        });
-    }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    userName,
+    userEmail,
+    role,
+    password: hashPassword,
+  });
 
-    const hashedPassword = await bcrypt.hash(password, 10); // salt value passed as 10
-    const newUser = new User({
-        userName,
-        userEmail,
-        role,
-        password: hashedPassword,
-    });
-    await newUser.save();
+  await newUser.save();
 
-    return res.status(201).json({
-        success: true,
-        message: 'User registered successfully'
-    });
+  return res.status(201).json({
+    success: true,
+    message: "User registered successfully!",
+  });
 };
 
-model.exports = {
-    registerUser,
+const loginUser = async (req, res) => {
+  const { userEmail, password } = req.body;
+
+  const checkUser = await User.findOne({ userEmail });
+
+  if (!checkUser || !(await bcrypt.compare(password, checkUser.password))) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+    });
+  }
+
+  const accessToken = jwt.sign(
+    {
+      _id: checkUser._id,
+      userName: checkUser.userName,
+      userEmail: checkUser.userEmail,
+      role: checkUser.role,
+    },
+    "JWT_SECRET",
+    { expiresIn: "120m" }
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Logged in successfully",
+    data: {
+      accessToken,
+      user: {
+        _id: checkUser._id,
+        userName: checkUser.userName,
+        userEmail: checkUser.userEmail,
+        role: checkUser.role,
+      },
+    },
+  });
 };
+
+module.exports = { registerUser, loginUser };
